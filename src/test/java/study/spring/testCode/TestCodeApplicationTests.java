70bd602823471617df47d5228c83675ef88de0a9
kimyubi <ieieie0419@gmail.com>
2023-05-23 18:29:33 +0900

    initial commit

---
+++ src/test/java/study/spring/testCode/TestCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.testCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
