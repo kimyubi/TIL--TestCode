5a75f4c37bb7df1cf207643168697b5cf51da74a
kimyubi <ieieie0419@gmail.com>
2023-05-29 13:53:08 +0900

    [TDD 3단계] REFACTORING : 기능을 보장하는 테스트 코드가 있으므로, 테스트 통과를 유지하며 과감한 리팩토링을 시도할 수 있다.

--- src/main/java/study/unit/CafeKiosk.java
+++ src/main/java/study/unit/CafeKiosk.java
@@ -60,10 +60,8 @@ public class CafeKiosk {
 	}
 
 	public int calculateTotalPrice() {
-		int totalPrice = 0;
-		for(Beverage beverage: beverageList){
-			totalPrice += beverage.getPrice();;
-		}
-		return totalPrice;
+		return beverageList.stream()
+			.mapToInt(Beverage :: getPrice)
+			.sum();
 	}
 }
